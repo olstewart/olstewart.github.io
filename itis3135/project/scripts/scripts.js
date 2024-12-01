d2894211809459770c8b96dba8e5226a2a1ec057
Onyae' Stewart <olstewart.06@gmail.com>
2024-11-30 23:53:31 -0500

    Added highlighting of images in JS

--- itis3135/project/scripts/scripts.js
+++ itis3135/project/scripts/scripts.js
@@ -1,1 +1,22 @@
-console.log("dam daddy.,.okay!!!")
+const projectImages = document.getElementsByClassName("project-image");
+const imageArr = Array.from(projectImages);
+const highlightedImage = document.getElementById("highlighted-image");
+
+const createHighlightedImage = (source) => {
+    highlightedImage.innerHTML = `
+        <img src="${source}">
+        <button id="delete-button">Exit</button><br>
+    `;
+    const deleteButton = document.getElementById("delete-button");
+    deleteButton.addEventListener("click", () => {
+        highlightedImage.innerHTML = "";
+    })
+};
+for(let i = 0; i < imageArr.length; i++){
+    console.log(imageArr[i].src);
+    const grog = imageArr[i].src;
+    imageArr[i].addEventListener("click", () => {
+        createHighlightedImage(grog);
+    });
+}
+
