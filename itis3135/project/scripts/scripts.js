af69660c000eb3870c23fdab3f0f01f189f1478d
Onyae' Stewart <olstewart.06@gmail.com>
2024-12-02 19:46:00 -0500

    Added dynamic header/footer, finished homepage

--- itis3135/project/scripts/scripts.js
+++ itis3135/project/scripts/scripts.js
@@ -19,3 +19,43 @@ for (let i = 0; i < imageArr.length; i++){
     });
 }
 
+// This code will make a gif of the gameplay play when the mouse glides over it.
+const infestedPreview = document.getElementById("infested-preview");
+const spacekidPreview = document.getElementById("spacekid-preview");
+const fluffballsPreview = document.getElementById("fluffballs-preview");
+
+const infestedSrc = infestedPreview.src;
+const spacekidSrc = spacekidPreview.src;
+const fluffballsSrc = fluffballsPreview.src;
+
+
+const changeImage = (originalImage, newSource) => {
+    originalImage.src = newSource;
+}
+
+//Infested
+infestedPreview.addEventListener("mouseover", ()=>{
+    changeImage(infestedPreview, "images/infested-gif-preview.gif");
+});
+
+infestedPreview.addEventListener("mouseleave", ()=>{
+    setTimeout(()=>{changeImage(infestedPreview, infestedSrc);}, 700);
+});
+
+//Spacekid
+spacekidPreview.addEventListener("mouseover", ()=>{
+    changeImage(spacekidPreview, "images/spacekid-gif-preview.gif");
+});
+
+spacekidPreview.addEventListener("mouseleave", ()=>{
+    setTimeout(()=>{changeImage(spacekidPreview, spacekidSrc);}, 700);
+});
+
+//Fluffballs
+fluffballsPreview.addEventListener("mouseover", ()=>{
+    changeImage(fluffballsPreview, "images/fluffballs-gif-preview.gif");
+});
+
+fluffballsPreview.addEventListener("mouseleave", ()=>{
+    setTimeout(()=>{changeImage(fluffballsPreview, fluffballsSrc);}, 700);
+});
